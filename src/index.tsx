19922b43e9d2a4853ad3fc6b5b0e303d7e6174f4
Mateusz Bochynski <mbochynski@pgs-soft.com>
2022-04-28 08:54:34 +0200

    Add xstate template

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { createMachine, assign } from "xstate";
+import { useMachine } from "@xstate/react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+interface ToggleContext {
+  count: number;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const toggleMachine = createMachine<ToggleContext>({
+  id: "toggle",
+  initial: "inactive",
+  context: {
+    count: 0,
+  },
+  states: {
+    inactive: {
+      on: { TOGGLE: "active" },
+    },
+    active: {
+      entry: assign({ count: (ctx) => ctx.count + 1 }),
+      on: { TOGGLE: "inactive" },
+    },
+  },
+});
+
+function App() {
+  const [current, send] = useMachine(toggleMachine);
+  const active = current.matches("active");
+  const { count } = current.context;
+
+  return (
+    <div className="App">
+      <h1>XState React Template</h1>
+      <h2>Fork this template!</h2>
+      <button onClick={() => send("TOGGLE")}>
+        Click me ({active ? "✅" : "❌"})
+      </button>{" "}
+      <code>
+        Toggled <strong>{count}</strong> times
+      </code>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
