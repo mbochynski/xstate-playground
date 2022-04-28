681d5757efef3d187a70a0cd0dcdb65dee45dda8
Mateusz Bochynski <mbochynski@pgs-soft.com>
2022-04-28 09:11:02 +0200

    Apply changes introduced by visual editor

--- src/index.tsx
+++ src/index.tsx
@@ -8,22 +8,30 @@ interface ToggleContext {
   count: number;
 }
 
-const toggleMachine = createMachine<ToggleContext>({
-  id: "toggle",
-  initial: "inactive",
-  context: {
-    count: 0,
-  },
-  states: {
-    inactive: {
-      on: { TOGGLE: "active" },
+const toggleMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcD2UoBswDoCWAdgIYDGyeAbmAMQAqA8gOKMAyAooqAA6qx7moCnEAA9EAWgCMAZgAsOWQAZZAJgAcATkmyAbIsUBWRToA0IAJ4SZGnAbUrF0wzrVrlygL4ezaDNhyk5FR0TKwcSCA8fAJCEWIIUnIKyupauvpGphYSDoo4atLSGhqKGioA7OU6kpJe3iAEqBBwwr5YuISBlGDCUfx4gsLx4k46OHIqRhUa5brS5WaWCdpjivaOOiolarLF5V4+6O0BZN29vP2DcYh6OBoG8+XSKi+79xqLVuWStuuyss9NJtFPt6m1sOdogNYqBhoU8hMpuUZnMFtkEhVpPk5IoVDonHZZJIVNI6h4gA */
+  createMachine<ToggleContext>({
+    context: { count: 0 },
+    id: "toggle",
+    initial: "inactive",
+    states: {
+      inactive: {
+        on: {
+          TOGGLE: {
+            target: "active",
+          },
+        },
+      },
+      active: {
+        entry: assign({ count: (ctx) => ctx.count + 1 }),
+        on: {
+          TOGGLE: {
+            target: "inactive",
+          },
+        },
+      },
     },
-    active: {
-      entry: assign({ count: (ctx) => ctx.count + 1 }),
-      on: { TOGGLE: "inactive" },
-    },
-  },
-});
+  });
 
 function App() {
   const [current, send] = useMachine(toggleMachine);
@@ -40,6 +48,9 @@ function App() {
       <code>
         Toggled <strong>{count}</strong> times
       </code>
+      <pre style={{ textAlign: "left" }}>
+        {JSON.stringify(current, null, 2)}
+      </pre>
     </div>
   );
 }
